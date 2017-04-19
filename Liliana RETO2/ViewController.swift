c0d930838e76a1c12bf939e68240bdd974b6b641
Liliana <lcpenab@MacBook-Air-de-Liliana.local>
2017-04-19 11:54:08 -0400

    Initial Commit

---
+++ Liliana RETO2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liliana RETO2
+//
+//  Created by Liliana on 4/19/17.
+//  Copyright © 2017 Liliana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
